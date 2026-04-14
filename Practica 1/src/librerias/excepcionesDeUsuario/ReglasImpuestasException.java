495dea8b2b06aea99a34a9274dacf7359d353dc5
Izan <>
2026-04-14 14:08:33 +0200

    arreglo organización

--- Practica 1/src/librerias/excepcionesDeUsuario/ReglasImpuestasException.java
+++
@@ -1,7 +0,0 @@
-package librerias.excepcionesDeUsuario;
-
-public class ReglasImpuestasException extends IllegalArgumentException {
-    public ReglasImpuestasException(String mensaje){
-        super(mensaje);
-    }
-}
